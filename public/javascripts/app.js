7fd7f57cf5f07efe654a80a397b4e5b8189ccf8b
bharat bhushan <bbbatra5@gmail.com>
2015-06-12 23:52:43 +0530

    initial setup

---
+++ public/javascripts/app.js
@@ -0,0 +1,71 @@
+var app = angular.module("blogscraper", [
+    "ui.router", "ngResource"
+]);
+
+app.config(["$stateProvider", "$locationProvider", "$urlRouterProvider",
+    function($stateProvider, $locationProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state("home", {
+                url: "/",
+                templateUrl: "/templates/home.html",
+                controller: ["$scope","Url",function($scope,Url) {
+                    $scope.processUrl = function(){
+                        Url.fetchList().then(function(data){
+                            $scope.data = data;
+                        },function(err){
+                            /*TODO handle Error*/
+                            console.log(err);
+                        })
+                       
+                    }
+                }]
+            })
+    }
+]);
+
+app.factory("UrlResource", ["$resource", function($resource) {
+    return $resource("/api/v1/url/:id", {
+        id: '@id'
+    }, {
+        update: {
+            method: "PUT"
+        },
+        delete: {
+            method: "DELETE"
+        }
+    });
+}]);
+
+app.factory("Url", ["$http", "$q", "UrlResource", function($http, $q, UrlResource) {
+    var list = [];
+    var selected;
+    return {
+        //TODO:TO be implemented when API is used.
+        fetchList: function() {
+            var defer = $q.defer();
+            try {
+                UrlResource
+                    .query({}, function(resp) {
+                        list = resp;
+                        defer.resolve(list);
+                    }, function(err) {
+                        list = [];
+                        defer.reject([]);
+                        console.log(err);
+                    });
+            } catch (e) {
+                console.log(e.stack);
+                list = [];
+                defer.reject([]);
+            }
+            return defer.promise;
+        },
+        getList: function() {
+            return list;
+        },
+        add: function(url) {
+            list.push(url);
+        }
+    }
+}]);
---
+++ routes/url.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var router = express.Router();
+var timers = require('timers') 
+var data = [{title:"Best blogpost ever",text:"Node.js is an open source, cross-platform runtime environment for server-side and networking applications. Node.js applications are written in JavaScript, and can be run within the Node.js runtime on OS X, Microsoft Windows, Linux, FreeBSD, NonStop and IBM i."}]
+
+
+/* GET processed url  listing. */
+router.get('/', function(req, res) {
+  console.log("called");
+  var id = timers.setTimeout(function(){
+    timers.clearTimeout(id);
+    res.json(data);
+  },5000)
+});
+router.get('/:id', function(req, res) {
+
+});
+
+
+module.exports = router;
